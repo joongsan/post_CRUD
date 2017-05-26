fddb5a1536eef4fe1ccc43c304238a2faea38e74
joongsan <pepejun123@hotmail.com>
2017-05-26 09:23:47 +0000

    added files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+    
+    before_action :authenticate_user!, except: [:index]
+    
+    def index
+        @posts = Post.all.reverse
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        post = Post.new
+        post.title = params[:title]
+        post.content = params[:content]
+        post.user_id = current_user.id
+        uploader = BrisbaneUploaderUploader.new
+        uploader.store!(params[:my_image])
+        post.image_url = uploader.url
+        post.tumb_url = uploader.thumb.url
+        post.save
+        redirect_to '/'
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        post = Post.find(params[:id])
+        post.title = params[:title]
+        post.content = params[:content]
+        post.save
+        
+        redirect_to '/'
+    end
+    
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+        
+        redirect_to '/'
+    end
+end
---
+++ db/migrate/20170505091307_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      
+      t.string  :title
+      t.text    :content
+      t.integer :user_id
+      t.string  :image_url
+      t.string  :tumb_url
+      
+      t.timestamps null: false
+    end
+  end
+end
